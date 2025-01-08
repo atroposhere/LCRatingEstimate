966cefde39f3ece9325d57be260b19b41c1d9b20
MountainsUsual <albertwangyf@foxmail.com>
2025-01-08 20:36:05 +0800

    refactor: add some parameter settings in main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 from wgd_estimate import estimate_score_with_wgd, read_data_from_csv, read_data_from_yaml
 from glicko_estimate import estimate_score_with_glicko
 
-# Constants
-INITIAL_SCORE = 1580
-LEARNING_RATE = 0.1
-MAX_ITER = 2000
-DECAY_FACTOR = 0.99
 DATA_FILE = './data/questions_data.yaml'  # or './data/questions_data.csv'
 # only for test
 # DATA_FILE = './data/test_data.csv'
 # DATA_FILE = './data/test_data.yaml'
-
-# Flag to decide which method to use
 USE_GLICKO = True  # Default to using Glicko method
+
+# for glicko method
+INITIAL_SCORE = 1580
+INITIAL_RD = 350
+INITIAL_VOL = 0.06
+
+# for WGD method.
+LEARNING_RATE = 0.1
+MAX_ITER = 2000
+DECAY_FACTOR = 0.99
 
 
 def main():
@@ -25,7 +28,8 @@ def main():
     if USE_GLICKO:
         # Estimate score using Glicko
         print("\nEstimating score using Glicko...")
-        estimated_score, confidence_interval = estimate_score_with_glicko(data)
+        estimated_score, confidence_interval = estimate_score_with_glicko(
+            data, initial_score=INITIAL_SCORE, initial_rd=INITIAL_RD, initial_volatility=INITIAL_VOL)
 
         # Output Glicko results
         print(f"\nGlicko Final Estimated Score: {estimated_score:.2f}")
